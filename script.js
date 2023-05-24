f9790d34268c3b334f81457dae0ecdc8d9aa340c
Jessie Gambellin <jessiegambellin@gmail.com>
2023-05-23 23:46:20 -0400

    logic working

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const computerChoiceOptions = ["rock", "paper", "scissors"]
 let playerScore = 0
 let computerScore = 0
+let choices = Array.from(document.getElementsByClassName('choice'));
+
+
 
 function getComputerChoice() {
     return computerChoiceOptions[Math.floor(Math.random() * computerChoiceOptions.length)];
@@ -36,18 +39,25 @@ function playGame(computerChoice, playerChoice) {
     }
 }
 
-function fullGame () {
-    for (let i = 0; i < 5; i++) {
-        let playerChoice = prompt('Please enter rock, paper, or scissors').toLowerCase()
-        let computerChoice = getComputerChoice()
-        let result = playGame(computerChoice, playerChoice);
-        console.log(result)
-    }
-    if (playerScore > computerScore) {
-        alert("You Win!")
-    } else if (computerScore > playerScore) {
-        alert("You Lose!")
-    }
-}
+choices.forEach(choice => {
+     choice.addEventListener('click', function(){
+        playGame(getComputerChoice(),choice.value)
+    })
+});
 
-fullGame();
+
+// function fullGame () {
+//     for (let i = 0; i < 5; i++) {
+//         let playerChoice = prompt('Please enter rock, paper, or scissors').toLowerCase()
+//         let computerChoice = getComputerChoice()
+//         let result = playGame(computerChoice, playerChoice);
+//         console.log(result)
+//     }
+//     if (playerScore > computerScore) {
+//         alert("You Win!")
+//     } else if (computerScore > playerScore) {
+//         alert("You Lose!")
+//     }
+// }
+
+// fullGame();
